5108a33fee27768742e1323c6029facdc2a2a6a8
fredslbn <fh.silaban@gmail.com>
2023-10-26 11:52:05 +0700

    update KSU

--- .circleci/violet2.sh
+++ .circleci/violet2.sh
@@ -177,7 +177,7 @@ function cloneTC() {
     elif [ $COMPILER = "clang17" ];
 	then
 	#git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/ -b android10-gsi --depth 1 --no-tags --single-branch clang_all	
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229.tar.gz && mkdir clang && tar -xzvf clang-r498229.tar.gz -C clang/
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz && mkdir clang && tar -xzvf clang-r498229b.tar.gz -C clang/
     #mv clang_all/clang-r353983c clang
     #rm -rf clang_all
     export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang"
@@ -202,7 +202,7 @@ function cloneTC() {
     elif [ $COMPILER = "clang17-7" ];
 	then
 	#git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/ -b android10-gsi --depth 1 --no-tags --single-branch clang_all	
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229.tar.gz && mkdir clang && tar -xzvf clang-r498229.tar.gz -C clang/
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz && mkdir clang && tar -xzvf clang-r498229b.tar.gz -C clang/
     #mv clang_all/clang-r353983c clang
     #rm -rf clang_all
     export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang"
